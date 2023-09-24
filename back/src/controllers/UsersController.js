789e9f49bf7e7f35e50a6cd0190085ac93c16ed3
wilterd <wilterd18@gmail.com>
2023-09-24 00:26:06 -0400

    fix erro controller users

--- back/src/controllers/UsersController.js
+++ back/src/controllers/UsersController.js
@@ -1,19 +1,15 @@
 const db = require('../config/config.js');
 
 
-
-//obtiene toas las materias
+//obtiene todas las materias
 const create_users = async (req, res) => {
+  db.serialize(() => {
+      db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), password VARCHAR(255))');
+  });
 
-
-    db.serialize(() => {
-        db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), password VARCHAR(255)');
-      
-      });
-
-    res.status(200).send({mensaje:'Administrador creado con exito'});
+  res.status(200).send({ mensaje: 'Administrador creado con éxito' });
 }
 
 module.exports = {
-    create_users
-                    };
+  create_users
+};
