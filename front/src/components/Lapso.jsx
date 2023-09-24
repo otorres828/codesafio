175080a67a69d4769b35eb2f4e2acdf72822bdd6
Oliver Torres <olivertorres1997@gmail.com>
2023-09-24 10:06:00 -0400

    ajuste

--- front/src/components/Lapso.jsx
+++ front/src/components/Lapso.jsx
@@ -1,28 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Card,
-  CardBody,
-  Typography,
-} from '@material-tailwind/react';
-import axios from '../api/axios';
 
-function Lapso({ title, subtitle, days }) {
-  const [notificaciones,setNotificaciones]=useState(null)
+import axios from '../api/axios';
+import { Dialog,DialogActions, DialogContent, DialogTitle } from "@mui/material";
+
+function Lapso() {
+  const [lapsos,setLapsos]=useState(null)
+  const [lapso,setLapso]=useState(null)
   const token_codesafio = localStorage.getItem("token_codesafio");
+  const [nombre, setNombre] = useState('');
+  const [lapso_id, setLapso_id] = useState('');
 
   const headers ={
     Accept: "application/json",
     "Content-Type": "application/json",
     "Authorization": `Bearer ${token_codesafio}`
   }
+
+  function obtener_lapsos() {
+    // axios
+    //   .get("lapsos/obtener", { headers: headers})
+    //   .then((response) => {
+    //     setLapsos(response.data);
+    //   });
+  }
+
+  function agregar_lapso(){
+    // axios
+    // .post("lapsos/actualizar",{nombre}, { headers: headers})
+    // .then((response) => {
+    //   obtener_lapsos()
+    // });
+  }
+
+  function actualizar(){
+    axios
+    .post("lapsos/actualizar/"+lapso_id,{nombre}, { headers: headers})
+    .then((response) => {
+      obtener_lapsos()
+    });
+  }
+
+
+  function handleClose(){
+      setOpen(false);
+  }
+
   useEffect(()=>{
-      //  axios.get('obtener_lapsos',{headers:headers})
-      //  .then((response)=>{
-      //     setNotificaciones(response.data)
-      //  })
+    // obtener_lapsos();
   },[])
+
+
   return (
-    <></>
+    <>
+
+
+    </>
   );
 }
 
